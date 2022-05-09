26344b5b046365fa05d64c9bc4ff08fad7336548
dhong639 <30446369+dhong639@users.noreply.github.com>
2022-05-09 11:24:08 -0500

    Add files via upload

---
+++ paths.py
@@ -0,0 +1,31 @@
+########################################
+# paths for running web server
+#		uncomment if running web server	
+#		commet if running tests
+########################################
+PATH_SAVES = 'save_files'
+PATH_CUSTOMER = 'save_files/{}'
+PATH_CUSTOMER_SAVE = 'save_files/{}/{}'
+PATH_CUSTOMER_SAVE_DETAILS = 'save_files/{}/{}/prescan_info/details.json'
+PATH_CUSTOMER_SAVE_HARDWARE = 'save_files/{}/{}/hardware-list.json'
+PATH_CUSTOMER_SAVE_SERVICE = 'save_files/{}/{}/service.json'
+PATH_CUSTOMER_TIMESTAMP_NAME = 'save_files/{}/timestamp-name.json'
+PATH_CUSTOMER_LOGS = 'save_files/{}/{}/logs'
+PATH_CUSTOMER_RAWS = 'save_files/{}/{}/raws'
+PATH_CUSTOMER_PRESCAN = 'save_files/{}/{}/prescan_info'
+PATH_CUSTOMER_PRIMARY = 'save_files/{}/{}/primary_scan_info'
+PATH_CURRENT_PREVIEW = 'app/static/javascript/current_preview'
+
+"""
+########################################
+# paths for running tests
+#		uncomment if running tests
+#		commet if running web server
+########################################
+PATH_SAVES = '../save_files'
+PATH_CUSTOMER = '../save_files/{}'
+PATH_CUSTOMER_TIMESTAMP_NAME = '../save_files/{}/timestamp-name.json'
+PATH_CUSTOMER_LOGS = '../save_files/{}/{}/logs'
+PATH_CUSTOMER_PRESCAN = '../save_files/{}/{}/prescan_info'
+PATH_CUSTOMER_PRIMARY = '../save_files/{}/{}/primary_scan_info'
+"""
