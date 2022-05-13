80a48c9b4a6f6ee07e5c70fef891f8fae9f09b4e
dhong639 <30446369+dhong639@users.noreply.github.com>
2022-05-13 15:08:18 -0500

    Update paths.py

--- paths.py
+++ paths.py
@@ -1,31 +1,16 @@
-########################################
-# paths for running web server
-#		uncomment if running web server	
-#		commet if running tests
-########################################
-PATH_SAVES = 'save_files'
-PATH_CUSTOMER = 'save_files/{}'
-PATH_CUSTOMER_SAVE = 'save_files/{}/{}'
-PATH_CUSTOMER_SAVE_DETAILS = 'save_files/{}/{}/prescan_info/details.json'
-PATH_CUSTOMER_SAVE_HARDWARE = 'save_files/{}/{}/hardware-list.json'
-PATH_CUSTOMER_SAVE_SERVICE = 'save_files/{}/{}/service.json'
-PATH_CUSTOMER_TIMESTAMP_NAME = 'save_files/{}/timestamp-name.json'
-PATH_CUSTOMER_LOGS = 'save_files/{}/{}/logs'
-PATH_CUSTOMER_RAWS = 'save_files/{}/{}/raws'
-PATH_CUSTOMER_PRESCAN = 'save_files/{}/{}/prescan_info'
-PATH_CUSTOMER_PRIMARY = 'save_files/{}/{}/primary_scan_info'
-PATH_CURRENT_PREVIEW = 'app/static/javascript/current_preview'
+import os
 
-"""
-########################################
-# paths for running tests
-#		uncomment if running tests
-#		commet if running web server
-########################################
-PATH_SAVES = '../save_files'
-PATH_CUSTOMER = '../save_files/{}'
-PATH_CUSTOMER_TIMESTAMP_NAME = '../save_files/{}/timestamp-name.json'
-PATH_CUSTOMER_LOGS = '../save_files/{}/{}/logs'
-PATH_CUSTOMER_PRESCAN = '../save_files/{}/{}/prescan_info'
-PATH_CUSTOMER_PRIMARY = '../save_files/{}/{}/primary_scan_info'
-"""
+PATH_ = os.getcwd()
+PATH_SAVES = os.path.join(PATH_, 'save_files')
+PATH_CUSTOMER = os.path.join(PATH_, 'save_files/{}')
+PATH_CUSTOMER_SAVE = os.path.join(PATH_, 'save_files/{}/{}')
+PATH_CUSTOMER_SAVE_DETAILS = os.path.join(PATH_, 'save_files/{}/{}/prescan_info/details.json')
+PATH_CUSTOMER_SAVE_HARDWARE = os.path.join(PATH_, 'save_files/{}/{}/hardware-list.json')
+PATH_CUSTOMER_SAVE_SERVICE = os.path.join(PATH_, 'save_files/{}/{}/service.json')
+PATH_CUSTOMER_TIMESTAMP_NAME = os.path.join(PATH_, 'save_files/{}/timestamp-name.json')
+PATH_CUSTOMER_LOGS = os.path.join(PATH_, 'save_files/{}/{}/logs')
+PATH_CUSTOMER_RAWS = os.path.join(PATH_, 'save_files/{}/{}/raws')
+PATH_CUSTOMER_PRESCAN = os.path.join(PATH_, 'save_files/{}/{}/prescan_info')
+PATH_CUSTOMER_PRIMARY = os.path.join(PATH_, 'save_files/{}/{}/primary_scan_info')
+PATH_CURRENT_PREVIEW = os.path.join(PATH_, 'app/static/javascript/current_preview')
+PATH_DOWNLOAD_DUMP = os.path.join(PATH_, 'app/generate_import_csv/download_dump/{}')
