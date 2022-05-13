0e93822be0cb16d91ca971c2ecfbf94f63b8dc7c
dhong639 <30446369+dhong639@users.noreply.github.com>
2022-05-13 15:09:18 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,5 +5,3 @@ from flask import Flask
 app = Flask(__name__)
 
 from app import routes
-#from miscellaneous import get_accessVLAN, get_listTrunkVLAN, get_trunkNativeVLAN, get_voiceVLAN
-#from paths import PATH_SAVES
