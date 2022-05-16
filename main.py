e92ab80615dffbb3bab09bbcd5125136661d7dbd
dhong639 <30446369+dhong639@users.noreply.github.com>
2022-05-16 16:01:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from app import app
+from paths import PATH_DOWNLOAD_DUMP
+
+import os
 
 
 if __name__ == '__main__':
+	# make directories if they don't already exist
+	if not os.path.exists(PATH_DOWNLOAD_DUMP.format('')):
+		os.mkdir(PATH_DOWNLOAD_DUMP.format(''))
+	if not os.path.exists(os.path.join(os.getcwd(), 'save_files')):
+		os.mkdir(os.path.join(os.getcwd(), 'save_files'))
+	
 	app.run(host='0.0.0.0', port=5000, debug=True)
