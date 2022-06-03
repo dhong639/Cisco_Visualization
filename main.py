c297b99b25553444ad6fb07c67ef1254f0585c8e
dhong639 <30446369+dhong639@users.noreply.github.com>
2022-06-03 11:57:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from app import app
-from paths import PATH_DOWNLOAD_DUMP
+from paths import PATH_DOWNLOAD_DUMP, PATH_CURRENT_PREVIEW
 
 import os
 
@@ -10,5 +10,7 @@ if __name__ == '__main__':
 		os.mkdir(PATH_DOWNLOAD_DUMP.format(''))
 	if not os.path.exists(os.path.join(os.getcwd(), 'save_files')):
 		os.mkdir(os.path.join(os.getcwd(), 'save_files'))
+	if not os.path.exists(PATH_CURRENT_PREVIEW):
+		os.mkdir(PATH_CURRENT_PREVIEW)
 	
 	app.run(host='0.0.0.0', port=5000, debug=True)
