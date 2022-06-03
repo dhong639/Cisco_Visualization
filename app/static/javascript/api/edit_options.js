0846898ab64b4f8b11e29a8be86048736f005049
dhong639 <30446369+dhong639@users.noreply.github.com>
2022-06-03 11:45:22 -0500

    Update edit_options.js

--- app/static/javascript/api/edit_options.js
+++ app/static/javascript/api/edit_options.js
@@ -49,3 +49,30 @@ function helper_site(elementID_select, elementID_pending, pending_set) {
 	list.sort()
 	document.getElementById(elementID_pending).innerHTML = list.join('<br />')
 }
+
+function set_serviceUp(set_currentSite, pending_serviceUp) {
+	var flag = false
+	var device_id = document.getElementById('select_detail_hostname').value
+	var interface_id = document.getElementById('select_detail_interface').value
+	set_currentSite.forEach(site_id => {
+		if(device_id in dict_devices[site_id]) {
+			if(dict_devices[site_id][device_id]['is_tor'] == true) {
+				if(device_id in pending_serviceUp == false) {
+					pending_serviceUp[device_id] = new Set()
+				}
+				if(pending_serviceUp[device_id].has(interface_id) == false) {
+					dict_edgeOther[device_id]['layer3'].push([
+						'undefined_serviceUplink',
+						interface_id
+					])
+					pending_serviceUp[device_id].add(interface_id)
+					flag = true
+				}
+			}
+		}
+	})
+	if(flag == true) {
+		load_graph(pending_device, pending_site, set_currentSite, graph)
+	}
+	console.log(pending_serviceUp)
+}
