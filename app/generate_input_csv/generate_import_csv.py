a243885aa78cc8f1acad6277dd6323ec69c34eee
dhong639 <30446369+dhong639@users.noreply.github.com>
2022-06-03 11:49:06 -0500

    Update generate_import_csv.py

--- app/generate_input_csv/generate_import_csv.py
+++ app/generate_input_csv/generate_import_csv.py
@@ -2,8 +2,10 @@ import json
 import os
 from .processInput import get_count_maxPort
 from .processInput import format_provisioning
+from .processInput import format_timestamp
 from paths import PATH_CUSTOMER_SAVE
 from paths import PATH_CUSTOMER_PRIMARY
+from paths import PATH_CUSTOMER_PRESCAN
 from .makeImport_service import makeImport_service
 from .makeImport_site import makeImport_site
 from .makeImport_servicePortProfile import makeImport_servicePortProfile
@@ -44,8 +46,8 @@ def get_list_provisioning(dict_device, dict_service, customer, timestamp, file_n
 	list_provisioning = []
 	with open(path_provisioning, 'r') as file:
 		list_provisioning = json.load(file)
-	# add names and port counts to provisioning (default settings, no name found)
-	format_provisioning(list_provisioning, dict_device, dict_service)
+	"""# add names and port counts to provisioning (default settings, no name found)
+	format_provisioning(list_provisioning, dict_device, dict_service)"""
 	return list_provisioning
 
 def get_list_portProfile(dict_device, dict_service, customer, timestamp):
@@ -54,10 +56,23 @@ def get_list_portProfile(dict_device, dict_service, customer, timestamp):
 def get_list_portSetting(dict_device, dict_service, customer, timestamp):
 	return get_list_provisioning(dict_device, dict_service, customer, timestamp, 'port-settings')
 
-def load_service_import(customer, timestamp, path_out):
+def load_service_import(customer, timestamp, path_out, incremental):
 	# create and download '1. Service Import.csv'
 	dict_service = get_dict_service(customer, timestamp)
-	makeImport_service(dict_service, path_out, 'v0.2', timestamp)
+	if incremental:
+		dict_ = {}
+		for site_id in dict_service:
+			for service_id in dict_service[site_id]:
+				for service_name in dict_service[site_id][service_id]:
+					if not dict_service[site_id][service_id][service_name]['base']:
+						service = dict_service[site_id][service_id][service_name]
+						if site_id not in dict_:
+							dict_[site_id] = {}
+						if service_id not in dict_[site_id]:
+							dict_[site_id][service_id] = {}
+						dict_[site_id][service_id][service_name] = service
+		dict_service = dict_
+	makeImport_service(dict_service, path_out, 'v0.2', format_timestamp(timestamp))
 
 def load_site_import(customer, timestamp, path_out):
 	# create and download '2. Sites Import.csv'
@@ -65,39 +80,56 @@ def load_site_import(customer, timestamp, path_out):
 	dict_service = get_dict_service(customer, timestamp)
 	makeImport_site(dict_device, dict_service, path_out, 'v0.2', timestamp)
 
-def load_servicePort_import(customer, timestamp, path_out):
+def load_servicePort_import(customer, timestamp, path_out, incremental):
 	# create and download '3. Service Ports Import.csv'
 	dict_device = get_dict_device(customer, timestamp)
 	dict_service = get_dict_service(customer, timestamp)
 	list_portProfile = get_list_portProfile(dict_device, dict_service, customer, timestamp)
-	makeImport_servicePortProfile(dict_service, list_portProfile, path_out, 'v0.2', timestamp)
+	if incremental:
+		list_portProfile = [profile for profile in list_portProfile if not profile['port_profile_name']]
+	# add names and port counts to provisioning (default settings, no name found)
+	format_provisioning(list_portProfile, dict_device, dict_service)
+	makeImport_servicePortProfile(dict_service, list_portProfile, path_out, 'v0.2', format_timestamp(timestamp))
 
-def load_ethPort_import(customer, timestamp, path_out):
+def load_ethPort_import(customer, timestamp, path_out, incremental):
 	# create and download '4. Eth-Port Profiles Import.csv'
 	dict_device = get_dict_device(customer, timestamp)
 	dict_service = get_dict_service(customer, timestamp)
 	list_portProfile = get_list_portProfile(dict_device, dict_service, customer, timestamp)
-	makeImport_ethPortProfile(dict_service, list_portProfile, path_out, 'v0.2', timestamp)
+	if incremental:
+		list_portProfile = [profile for profile in list_portProfile if not profile['port_profile_name']]
+	# add names and port counts to provisioning (default settings, no name found)
+	format_provisioning(list_portProfile, dict_device, dict_service)
+	makeImport_ethPortProfile(dict_service, list_portProfile, path_out, 'v0.2', format_timestamp(timestamp))
 
-def load_portSettings_import(customer, timestamp, path_out):
+def load_portSettings_import(customer, timestamp, path_out, incremental):
 	# create and download '5. Eth-Port Settings Import.csv'
 	dict_device = get_dict_device(customer, timestamp)
 	dict_service = get_dict_service(customer, timestamp)
 	list_portSetting = get_list_portSetting(dict_device, dict_service, customer, timestamp)
-	makeImport_ethPortSetting(dict_service, list_portSetting, path_out, 'v0.2', timestamp)
+	if incremental:
+		list_portSetting = [setting for setting in list_portSetting if not setting['port_setting_name']]
+	# add names and port counts to provisioning (default settings, no name found)
+	format_provisioning(list_portSetting, dict_device, dict_service)
+	makeImport_ethPortSetting(dict_service, list_portSetting, path_out, 'v0.2', format_timestamp(timestamp))
 
 def load_endpoints_import(customer, timestamp, path_out):
 	# create and download '6. Switch Endpoints Import.csv'
 	dict_device = get_dict_device(customer, timestamp)
 	port_count = get_count_maxPort(dict_device)
-	makeImport_endpoint(dict_device, port_count, path_out, 'v0.2', timestamp)
+	makeImport_endpoint(dict_device, port_count, path_out, 'v0.2', format_timestamp(timestamp))
 
 def load_lags_import(customer, timestamp, path_out):
 	# create and download '7. LAGs Import.csv'
 	dict_device = get_dict_device(customer, timestamp)
 	dict_service = get_dict_service(customer, timestamp)
 	list_portProfile = get_list_portProfile(dict_device, dict_service, customer, timestamp)
-	makeImport_lag(dict_device, list_portProfile, path_out, 'v0.2', timestamp)
+	dict_edgesFabric = {}
+	path_prescan = PATH_CUSTOMER_PRESCAN.format(customer, timestamp)
+	with open(os.path.join(path_prescan, 'graph_fabric.json'), 'r') as file:
+		dict_edgesFabric = json.load(file)
+	format_provisioning(list_portProfile, dict_device, dict_service)
+	makeImport_lag(dict_device, list_portProfile, dict_edgesFabric, path_out, 'v0.2', format_timestamp(timestamp))
 
 def load_deviceSettings_import(customer, timestamp, path_out):
 	# create and download '8. Device Settings Import.csv'
@@ -108,12 +140,14 @@ def load_bundles_import(customer, timestamp, path_out):
 	dict_device = get_dict_device(customer, timestamp)
 	dict_service = get_dict_service(customer, timestamp)
 	list_portProfile = get_list_portProfile(dict_device, dict_service, customer, timestamp)
+	format_provisioning(list_portProfile, dict_device, dict_service)
 	list_portSetting = get_list_portSetting(dict_device, dict_service, customer, timestamp)
+	format_provisioning(list_portSetting, dict_device, dict_service)
 	port_count = get_count_maxPort(dict_device)
-	makeImport_bundle(dict_device, list_portProfile, list_portSetting, port_count, path_out, 'v0.2', timestamp)
+	makeImport_bundle(dict_device, list_portProfile, list_portSetting, port_count, path_out, 'v0.2', format_timestamp(timestamp))
 
-def load_controllers_import(customer, timestamp, path_out, sdlc, ssh_username, ssh_password):
+def load_controllers_import(customer, timestamp, path_out, sdlc, enable, ssh_username, ssh_password):
 	# create and download '10. Controllers Import.csv'
 	dict_device = get_dict_device(customer, timestamp)
 	dict_service = get_dict_service(customer, timestamp)
-	makeImport_controller(dict_device, dict_service, sdlc, ssh_username, ssh_password, path_out, 'v0.10', timestamp)
+	makeImport_controller(dict_device, dict_service, sdlc, enable, ssh_username, ssh_password, path_out, 'v0.10', format_timestamp(timestamp))
