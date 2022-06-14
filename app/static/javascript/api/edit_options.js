e32703e26d18e59a37d07f5ee63c340c5ec3cf74
dhong639 <30446369+dhong639@users.noreply.github.com>
2022-06-14 14:33:48 -0500

    Update edit_options.js

--- app/static/javascript/api/edit_options.js
+++ app/static/javascript/api/edit_options.js
@@ -50,29 +50,104 @@ function helper_site(elementID_select, elementID_pending, pending_set) {
 	document.getElementById(elementID_pending).innerHTML = list.join('<br />')
 }
 
-function set_serviceUp(set_currentSite, pending_serviceUp) {
+function add_uplinkSingle(pending_uplinkSingle) {
+	var device_id = document.getElementById('select_uplinkTOR').value
+	var interface_id = document.getElementById('select_add_uplinkSingle').value
+	/**
+	 * given interface can only be a service_up in the following situations: 
+	 * 		not a fabric port
+	 * if conditions are satisfied, add to pending_serviceUp
+	 * if interface is an eth, the interface is overriden
+	 */
 	var flag = false
-	var device_id = document.getElementById('select_detail_hostname').value
-	var interface_id = document.getElementById('select_detail_interface').value
-	set_currentSite.forEach(site_id => {
-		if(device_id in dict_devices[site_id]) {
-			if(dict_devices[site_id][device_id]['is_tor'] == true) {
-				if(device_id in pending_serviceUp == false) {
-					pending_serviceUp[device_id] = new Set()
-				}
-				if(pending_serviceUp[device_id].has(interface_id) == false) {
-					dict_edgeOther[device_id]['layer3'].push([
-						'undefined_serviceUplink',
-						interface_id
-					])
-					pending_serviceUp[device_id].add(interface_id)
-					flag = true
-				}
+	for(var target_id in dict_edgeFabric[device_id]) {
+		dict_edgeFabric[device_id][target_id]['pairs'].forEach(pair => {
+			if(pair[0] != null && interface_id == pair[0]) {
+				flag= true
 			}
-		}
-	})
-	if(flag == true) {
-		load_graph(pending_device, pending_site, set_currentSite, graph)
+		})
 	}
-	console.log(pending_serviceUp)
+	if(flag == false) {
+		if(device_id in pending_uplinkSingle == false) {
+			pending_uplinkSingle[device_id] = new Set()
+		}
+		pending_uplinkSingle[device_id].add(interface_id)
+		var string = ''
+		for(var key in pending_uplinkSingle) {
+			string += key + '<br />'
+			pending_uplinkSingle[key].forEach(value => {
+				string += ' - ' + value + '<br />' 
+			})
+		}
+		document.getElementById('pending_uplinkSingle').innerHTML = string
+	}
+}
+
+function add_uplinkMultiple(pending_uplinkMultiple, set_currentSite) {
+	//	acquire search string and clear value from input
+	var string = document.getElementById('string_add_uplinkMultiple').value.toLowerCase()
+	document.getElementById('string_add_uplinkMultiple').value = ''
+
+	//	search string may be deleted later
+	var select_del_uplinkMultiple = document.getElementById('select_del_uplinkMultiple')
+	var set_options = new Set()
+	for(var i = 0; i<select_del_uplinkMultiple.length; i++) {
+		set_options.add(select_del_uplinkMultiple.options[i].text)
+	}
+	if(set_options.has(string) == false) {
+		var option = document.createElement('option')
+		option.text = string
+		option.value = string
+		select_del_uplinkMultiple.add(option)
+	}
+	
+	/**
+	 * 	for each currently loaded site, set the search string
+	 * 	doesn't do anything now, only displayed
+	 * 	will eventually search for string in port descriptions
+	 */
+	set_currentSite.forEach(site_id => {
+		if(site_id in pending_uplinkMultiple == false) {
+			pending_uplinkMultiple[site_id] = new Set()
+		}
+		pending_uplinkMultiple[site_id].add(string)
+	})
+	var string = ''
+	for(var key in pending_uplinkMultiple) {
+		string += key + '<br />'
+		pending_uplinkMultiple[key].forEach(value => {
+			string += ' - ' + value + '<br />'
+		})
+	}
+	document.getElementById('pending_uplinkMultiple').innerHTML = string
+}
+
+function del_uplinkMultiple(pending_uplinkMultiple) {
+	//	get value of string to be deleted and remove it from select element
+	var select_del_uplinkMultiple = document.getElementById('select_del_uplinkMultiple')
+	if(select_del_uplinkMultiple.selectedIndex != 0) {
+		var removed_string = select_del_uplinkMultiple.value
+		select_del_uplinkMultiple.remove(select_del_uplinkMultiple.selectedIndex)
+		pending_removedString.add(removed_string)
+		/**
+		 * 	remove string from all sites to be searched
+		 * 	at same time, update pending text
+		 * 	these items can be done concurrently
+		 */
+		var string = ''
+		for(var site_id in pending_uplinkMultiple) {
+			//	Set definition return false when removing non-existent elements
+			pending_uplinkMultiple[site_id].delete(removed_string)
+			string += site_id + '<br />'
+			pending_uplinkMultiple[site_id].forEach(value => {
+				string += ' - ' + value + '<br />'
+			})
+		}
+		document.getElementById('pending_uplinkMultiple').innerHTML = string
+		var string = ''
+		pending_removedString.forEach(item => {
+			string += ' - ' + item + '<br />'
+		})
+		document.getElementById('pending_removedString').innerHTML = string
+	}
 }
